72643df8bd497ebf0d33af68a9ef9fe527b07a32
GuillemRX <guillem.raja@esedsl.com>
2021-10-15 10:32:28 +0200

    Create ISOTest.sol

---
+++ ISOTest.sol
@@ -0,0 +1,35 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract ISODeployer {
+
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    struct ISO {
+        uint256 creationDate;
+        uint256 updateDate;
+        uint8 numUpdates;
+    }
+
+    mapping (uint8 => uint8[]) private enterprises;
+    
+    
+    function addISO(uint8 enterprise, uint8 num) 
+    public 
+    {
+        enterprises[enterprise].push(num);
+    }
+    
+    
+    function getEnterpriseISOS(uint8 enterprise) 
+    public 
+    returns(uint8[] memory)
+    {
+        return enterprises[enterprise];
+    }
+    
+}
